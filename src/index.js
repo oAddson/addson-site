11aa205a0d19e2230690b3d7e91fd54b8717d6d9
Addson Coutinho <addsoncoutinho@hotmail.com>
2021-03-20 03:05:02 -0300

    miragejs installed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import "./bootstrap.scss";
+//import "./vendor";
 
 const header = document.querySelector('header');
 
 
 header.style.backgroundColor = '#FF0000';
 
-header.addEventListener('click', () =>{
-    console.log('babel?')
-});
+const doGet = (url) => {
+    const promiseCallback = (resolve, reject) => {
+    fetch(url)
+        .then((response) => { 
+            if(!response.ok) throw new Error ('Deu ruim'); 
+            return response.json();  
+    })
+        .then(resolve)
+        .catch(reject)
+    }
+    return new Promise(promiseCallback);
+}
+
+
+setTimeout(() => {
+    doGet(window.location.href + 'api/movies').then(el => {
+        console.log('Data from Mirage Api:')
+        el.movies.forEach(
+            movie => console.log('*' + movie.name)
+        )
+    })
+}, 500);
+
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
     mode: 'development',
+    entry: ['./src/index.js','./src/server.js'],
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist'),
