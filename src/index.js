d4e4d91b4c280bd87f91450fe7b9b02ed25a0a3c
Addson Coutinho <addsoncoutinho@hotmail.com>
2021-03-26 16:47:48 -0300

    react is working on master

--- src/index.js
+++ src/index.js
@@ -5,4 +5,28 @@ import App from "./js/App";
 import "./bootstrap.scss";
 import "./vendor";
 
+const doGet = (url) => {
+    const promiseCallback = (resolve, reject) => {
+    fetch(url)
+        .then((response) => { 
+            if(!response.ok) throw new Error ('Deu ruim'); 
+            return response.json();  
+    })
+        .then(resolve)
+        .catch(reject)
+    }
+    return new Promise(promiseCallback);
+}
+
+
+setTimeout(() => {
+    doGet(window.location.href + 'api/movies').then(el => {
+        console.log('Data from Mirage Api:')
+        el.movies.forEach(
+            movie => console.log('*' + movie.name)
+        )
+    })
+}, 500);
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
